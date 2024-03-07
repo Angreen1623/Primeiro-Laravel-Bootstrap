9e4219e094c182d354a6932fa93ab0c4d482e54c
angreen1623 <andreopaim@gmail.com>
2024-03-07 11:55:23 -0300

    Adicionando a tela listar

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -13,8 +13,8 @@
 
     <div class="container">
         <div class="row">
-            <div class="col">
-                <nav class="navbar navbar-expand-lg bg-primary mb-4" data-bs-theme="dark">
+            <div class="col px-0">
+                <nav class="navbar navbar-expand-lg bg-primary mb-4 " data-bs-theme="dark">
                     <div class="container-fluid">
                         <a class="navbar-brand" href="#">SISTEMA WEB</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,7 +26,7 @@
                                     <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="listar">Consultar</a>
+                                    <a class="nav-link" href="#">Consultar</a>
                                 </li>
                             </ul>
                         </div>
@@ -35,44 +35,20 @@
             </div>
         </div>
         <div class="row">
-            <div class="col">
+            <div class="col px-0">
                 <p class="mb-3 fs-3">Cadastrar - Agendamento de Potenciais Clientes</p>
                 <p class="mb-3 fs-5">Sistema Utilizado para agendamento de serviços.</p>
                 
             </div>
         </div>
-        <div class="row">
-            <div class="col">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Nome:</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput1">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Telefone:</label>
-                    <input type="text" class="form-control" id="exampleFormControlInput2">
-                </div>
-                <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Origem:</label>
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Celular</option>
-                    <option value="1">Fixo</option>
-                    <option value="2">Residencial</option>
-                </select>
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Data do Contato:</label>
-                    <input type="date" class="form-control" id="exampleFormControlInput4" placeholder="02/02/2024">
-                </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">Observação:</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="5"></textarea>
-                </div>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col">
-                <button type="button" class="btn btn-primary">Cadastrar</button>
-            </div>
+        <div class="row text-center bg-primary text-bg-primary">
+            <div class="col">nome</div> 						
+            <div class="col">endereço</div>
+            <div class="col">bairro</div>
+            <div class="col">cep</div>
+            <div class="col">cidade</div>
+            <div class="col">estado</div>
+            <div class="col">ação</div>
         </div>
     </div>
     
