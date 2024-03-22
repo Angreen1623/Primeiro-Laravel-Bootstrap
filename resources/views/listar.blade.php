1c8da12296e43a7c4d5151138ad1157209a5c867
angreen1623 <andreopaim@gmail.com>
2024-03-22 10:23:28 -0300

    Corrigindo o link

--- resources/views/listar.blade.php
+++ resources/views/listar.blade.php
@@ -16,14 +16,14 @@
             <div class="col px-0">
                 <nav class="navbar navbar-expand-lg bg-primary mb-4 " data-bs-theme="dark">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="index">SISTEMA WEB</a>
+                        <a class="navbar-brand" href="/">SISTEMA WEB</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
                         <div class="collapse navbar-collapse" id="navbarNav">
                             <ul class="navbar-nav">
                                 <li class="nav-item">
-                                    <a class="nav-link" aria-current="page" href="index">Cadastrar</a>
+                                    <a class="nav-link" aria-current="page" href="/">Cadastrar</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link active" href="listar">Consultar</a>
